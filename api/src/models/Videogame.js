4185a49471c3ecf5db5d0dda4faa79d4433b2850
William Garcia <william_garcia_m@outlook.com>
2022-08-04 21:41:50 -0500

    timestamps-to-false-model-videogame

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -3,44 +3,50 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("videogame", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
+  sequelize.define(
+    "videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
 
-    name: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-    },
+      name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
 
-    description: {
-      types: DataTypes.STRING(500),
-      allowNull: false,
-    },
+      description: {
+        types: DataTypes.STRING(500),
+        allowNull: false,
+      },
 
-    released: {
-      types: DataTypes.STRING(10),
-    },
+      released: {
+        types: DataTypes.STRING(10),
+      },
 
-    rating: {
-      types: DataTypes.FLOAT,
-    },
+      rating: {
+        types: DataTypes.FLOAT,
+      },
 
-    platforms: {
-      types: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: false,
-    },
+      platforms: {
+        types: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
 
-    image: {
-      types: DataTypes.STRING,
-    },
+      image: {
+        types: DataTypes.STRING,
+      },
 
-    createdInDb: {
-      types: DataTypes.BOOLEAN,
-      defaultValue: true,
-      allowNull: false,
+      createdInDb: {
+        types: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
