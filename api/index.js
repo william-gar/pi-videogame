25f1195ed8806e75cb13dc01b52b9c5f3465473b
William Garcia <william_garcia_m@outlook.com>
2022-08-14 00:08:12 -0500

    first-save-genres-database

--- api/index.js
+++ api/index.js
@@ -17,12 +17,15 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const axios = require("axios");
+const { loadGenres } = require("./src/controllers/genres.controllers");
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then(async () => {
+  await loadGenres(true);
   server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
