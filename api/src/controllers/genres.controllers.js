6801e9d6168a6e5a27acd8dc6ea596aadf71665a
William Garcia <william_garcia_m@outlook.com>
2022-08-13 23:52:42 -0500

    get-genres-init

--- api/src/controllers/genres.controllers.js
+++ api/src/controllers/genres.controllers.js
@@ -1,9 +1,36 @@
 const axios = require("axios");
+const { Genre } = require("../db");
+require("dotenv").config();
+const { API_KEY } = process.env;
 
-const getGenres = async (req, res) => {
-  return res.send("GET GENRES ROUTE");
+const loadGenres = async (db = false) => {
+  let genres = [];
+
+  const api = await axios(`https://api.rawg.io/api/genres?key=${API_KEY}`);
+
+  genres = [...api.data.results];
+
+  // Filter - Info
+  genres = genres.map((genre) => {
+    const { id, name } = genre;
+    const obj = {
+      id,
+      name,
+    };
+
+    return obj;
+  });
+
+  if (db) await Genre.bulkCreate(genres);
+
+  return genres;
 };
 
+// GET - GENRES
+const getGenres = async (req, res) => {
+  //
+};
 module.exports = {
+  loadGenres,
   getGenres,
 };
