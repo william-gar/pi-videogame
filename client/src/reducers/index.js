ac8ef83f7200d2bc1d2f8de787b321ab783947ba
William Garcia <william_garcia_m@outlook.com>
2022-08-22 00:37:42 -0500

    reducers-get-videogames

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,19 @@
-function rootReducer() {}
+const initialState = {
+  videogames: [],
+  genres: [],
+};
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "GET_VIDEOGAMES":
+      return {
+        ...state,
+        videogames: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
 
 export default rootReducer;
