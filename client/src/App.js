2588f15231f867faf8e64c11568c5c93453bce1e
William Garcia <william_garcia_m@outlook.com>
2022-08-22 00:39:04 -0500

    app-landingPage-and-home-components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1>Henry Videogames</h1>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/home" component={Home} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
