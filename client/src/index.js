6f91d7a287615bc34084ced3a7dc7c40a62bdf6b
William Garcia <william_garcia_m@outlook.com>
2022-08-22 00:40:26 -0500

    index-provider-and-store

--- client/src/index.js
+++ client/src/index.js
@@ -11,8 +11,9 @@ ReactDOM.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-    , document.getElementById("root")
-  </Provider>
+  </Provider>,
+
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
