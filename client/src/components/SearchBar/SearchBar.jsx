00dd5e1b29452ae1882c27146339700dcaf3ba90
William Garcia <william_garcia_m@outlook.com>
2022-08-23 19:17:45 -0500

    searchBar-component-and-styles

---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getVideogamesByName } from "../../actions";
+import style from "./SearchBar.module.css";
+
+export default function SearchBar() {
+  const dispatch = useDispatch();
+  const [nameVideogame, setNameVideogame] = useState("");
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setNameVideogame(e.target.value);
+    // console.log(nameVideogame);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(getVideogamesByName(nameVideogame));
+  };
+  return (
+    <div className={style.searchBar}>
+      <input
+        className={style.search}
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => handleInputChange(e)}
+      ></input>
+      <input
+        type="submit"
+        value="&#128269;"
+        className={style.submit}
+        onClick={(e) => handleSubmit(e)}
+      ></input>
+    </div>
+  );
+}
