69d6e718c4df808b168d930f931e9d10e659b28a
William Garcia <william_garcia_m@outlook.com>
2022-08-24 00:37:36 -0500

    add-route_home_id-and-exact-in-route_home

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Home from "./components/Home/Home";
+import Detail from "./components/Detail/Detail";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route path="/home" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route path="/home/:id" component={Detail} />
         </Switch>
       </div>
     </BrowserRouter>
