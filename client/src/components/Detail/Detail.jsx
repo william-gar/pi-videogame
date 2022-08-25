7d4a30b5eeff156e9e2a2028db25899407b4354d
William Garcia <william_garcia_m@outlook.com>
2022-08-25 13:08:20 -0500

    almost-finished-component-Detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetailVideogame, resetDetail } from "../../actions";
 import { useEffect } from "react";
+import style from "./Detail.module.css";
 const stars = ["⭐", "⭐", "⭐", "⭐", "⭐"];
 
 export default function Detail(props) {
-  console.log(props);
+  // console.log(props);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,21 +20,56 @@ export default function Detail(props) {
     dispatch(resetDetail());
   }
 
-  const { id, name, description, released, rating, platforms, image, genres } =
+  const { name, description, released, rating, platforms, image, genres } =
     videogame;
 
   return (
-    <div>
+    <div className={style.componentDetail}>
       <Link to="/home">
-        <button className="goBackDetail" onClick={() => handleResetDetail()}>
+        <button
+          className={style.goBackDetail}
+          onClick={() => handleResetDetail()}
+        >
           &#8592; Go Back
         </button>
       </Link>
 
-      {videogame ? (
-        <div>
-          <h1>{name}</h1>
-          <img src={image} alt={`pic-${name}`} />
+      {image ? (
+        <div className={style.containerDetail}>
+          <div className={style.imageDetail}>
+            <img src={image} alt={`pic-${name}`} />
+          </div>
+          <div className={style.infoDetail}>
+            <div className={style.nameDetail}>
+              <h1>{name}</h1>
+            </div>
+            <div className={style.ratingDetail}>
+              <h3>&nbsp;{rating}</h3>
+              <h4>
+                {rating ? stars.slice(0, Math.floor(rating)).join("") : `---`}
+              </h4>
+            </div>
+            <div className={style.genresDetail}>
+              <h3>
+                <span>Genres:</span> {genres}
+              </h3>
+            </div>
+            <div className={style.containerDescriptionDetail}>
+              <div className={style.descriptionDetail}>
+                <p>{description}</p>
+              </div>
+            </div>
+            <div className={style.releasedDetail}>
+              <h4>
+                <span>Released:</span> {released}
+              </h4>
+            </div>
+            <div className={style.platformsDetail}>
+              <h4>
+                <span>Platforms:</span> {platforms}
+              </h4>
+            </div>
+          </div>
         </div>
       ) : (
         <h2>Loading...</h2>
