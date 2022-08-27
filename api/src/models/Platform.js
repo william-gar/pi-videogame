07418bf1fb36b0e05f6c040a583aa379ed8417e2
William Garcia <william_garcia_m@outlook.com>
2022-08-27 13:49:02 -0500

    add-Platforms-model-in-database

---
+++ api/src/models/Platform.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define(
+    "platform",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
