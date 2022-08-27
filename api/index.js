4be9a4bc839db5898b0d1ba4363d01d9af4030b2
William Garcia <william_garcia_m@outlook.com>
2022-08-27 13:57:41 -0500

    add-loadPlatforms-to_save_platforms_info-in-database

--- api/index.js
+++ api/index.js
@@ -21,10 +21,12 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 // const axios = require("axios");
 const { loadGenres } = require("./src/controllers/genres.controllers");
+const { loadPlatforms } = require("./src/controllers/platforms.controllers");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
   await loadGenres(true);
+  await loadPlatforms(true);
   server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
