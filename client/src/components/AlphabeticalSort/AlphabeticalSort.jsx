35a18e35ee11a51b6d4eb24576ea03421a483dba
William Garcia <william_garcia_m@outlook.com>
2022-08-30 23:39:04 -0500

    modularized-by-component-alphabeticalSort

---
+++ client/src/components/AlphabeticalSort/AlphabeticalSort.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function AlphabeticalSort({ handleAlphabeticalSort }) {
+  return (
+    <>
+      <select
+        defaultValue="default"
+        onChange={(e) => handleAlphabeticalSort(e, 1)}
+      >
+        <option disabled value="default">
+          Alphabetical Sort
+        </option>
+        <option value="a-z">A - Z</option>
+        <option value="z-a">Z - A</option>
+      </select>
+    </>
+  );
+}
