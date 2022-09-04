7fe475ad850d9590099222a06ad517bd254269d4
William Garcia <william_garcia_m@outlook.com>
2022-09-03 21:32:09 -0500

    add-submit_with_enter-in-SearchBar-and-modified-styles

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -10,31 +10,34 @@ export default function SearchBar({ handleSetCurrentPage }) {
   const handleInputChange = (e) => {
     e.preventDefault();
     setNameVideogame(e.target.value);
-    // console.log(nameVideogame);
   };
 
   const handleSubmit = (e) => {
-    handleSetCurrentPage();
-    dispatch(resetVideogames());
     e.preventDefault();
-    dispatch(getVideogamesByName(nameVideogame.trim()));
-    setNameVideogame("");
+
+    if (nameVideogame.trim().length) {
+      handleSetCurrentPage();
+      dispatch(resetVideogames());
+      dispatch(getVideogamesByName(nameVideogame.trim()));
+      setNameVideogame("");
+    }
   };
+
   return (
     <div className={style.searchBar}>
-      <input
-        className={style.search}
-        value={nameVideogame}
-        type="text"
-        placeholder="Search..."
-        onChange={(e) => handleInputChange(e)}
-      ></input>
-      <input
-        type="submit"
-        value="&#128269;"
-        className={style.submit}
-        onClick={(e) => handleSubmit(e)}
-      ></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          className={style.search}
+          value={nameVideogame}
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => handleInputChange(e)}
+          required
+        ></input>
+        <button type="submit" className={style.submit}>
+          &#128269;
+        </button>
+      </form>
     </div>
   );
 }
