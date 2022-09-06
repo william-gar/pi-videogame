ce34d69a41da813d84c8ebc3d4bf598df9b9f770
William Garcia <william_garcia_m@outlook.com>
2022-09-06 14:22:34 -0500

    change-normal_functions-to-arrow_functions-and-axios-to-fetch

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 import {
   GET_VIDEOGAMES,
   GET_GENRES,
@@ -16,129 +16,109 @@ import {
   FROM_BY_NAME_TO_ALLVIDEOGAMES,
 } from "../types";
 
-export function getVideogames() {
-  return async function (dispatch) {
-    let json = await axios(`/videogames`);
+// import dotenv from "dotenv";
+// dotenv.config();
 
-    return dispatch({
-      type: GET_VIDEOGAMES,
-      payload: json.data,
-    });
-  };
-}
+const BASE_URL = process.env.REACT_APP_API;
+const LOCALHOST = "http://localhost:3001";
 
-export function getGenres() {
-  return async function (dispatch) {
-    let genres = await axios(`/genres`);
+export const getVideogames = () => (dispatch) => {
+  fetch(`${BASE_URL || LOCALHOST}/videogames`)
+    .then((response) => response.json())
+    .then((data) =>
+      dispatch({
+        type: GET_VIDEOGAMES,
+        payload: data,
+      })
+    );
+};
 
-    return dispatch({
-      type: GET_GENRES,
-      payload: genres.data,
-    });
-  };
-}
+export const getGenres = () => (dispatch) => {
+  fetch(`${BASE_URL || LOCALHOST}/genres`)
+    .then((response) => response.json())
+    .then((data) =>
+      dispatch({
+        type: GET_GENRES,
+        payload: data,
+      })
+    );
+};
 
-export function filterByGenre(payload) {
-  return {
-    type: FILTER_BY_GENRE,
-    payload,
-  };
-}
+export const getPlatforms = () => (dispatch) => {
+  fetch(`${BASE_URL || LOCALHOST}/platforms`)
+    .then((response) => response.json())
+    .then((data) =>
+      dispatch({
+        type: GET_PLATFORMS,
+        payload: data,
+      })
+    );
+};
 
-export function filterByApiOrDb(payload) {
-  return {
-    type: FILTER_BY_API_OR_DB,
-    payload,
-  };
-}
+export const getVideogamesByName = (name) => (dispatch) => {
+  fetch(`${BASE_URL || LOCALHOST}/videogames?name=${name}`)
+    .then((response) => response.json())
+    .then((data) =>
+      dispatch({
+        type: GET_VIDEOGAMES_BY_NAME,
+        payload: data,
+      })
+    );
+};
 
-export function alphabeticalSort(payload) {
-  return {
-    type: ALPHABETICAL_SORT,
-    payload,
-  };
-}
+export const getDetailVideogame = (id) => (dispatch) => {
+  try {
+    fetch(`${BASE_URL || LOCALHOST}/videogames/${id}`)
+      .then((response) => response.json())
+      .then((data) =>
+        dispatch({
+          type: GET_DETAIL_VIDEOGAME,
+          payload: data,
+        })
+      );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export function sortByRating(payload) {
-  return {
-    type: SORT_BY_RATING,
-    payload,
-  };
-}
+export const postVideogame = (payload) => (dispatch) => {
+  fetch(`${BASE_URL || LOCALHOST}/videogames`, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
-export function getVideogamesByName(name) {
-  return async function (dispatch) {
-    let videogamesByName = await axios(`/videogames?name=${name}`);
+export const filterByGenre = (payload) => ({ type: FILTER_BY_GENRE, payload });
 
-    return dispatch({
-      type: GET_VIDEOGAMES_BY_NAME,
-      payload: videogamesByName.data,
-    });
-  };
-}
+export const filterByApiOrDb = (payload) => ({
+  type: FILTER_BY_API_OR_DB,
+  payload,
+});
 
-export function getDetailVideogame(id) {
-  return async function (dispatch) {
-    try {
-      let videogameById = await axios.get(`/videogames/${id}`);
+export const alphabeticalSort = (payload) => ({
+  type: ALPHABETICAL_SORT,
+  payload,
+});
 
-      return dispatch({
-        type: GET_DETAIL_VIDEOGAME,
-        payload: videogameById.data,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-}
+export const sortByRating = (payload) => ({
+  type: SORT_BY_RATING,
+  payload,
+});
 
-export function resetDetail() {
-  return {
-    type: RESET_DETAIL,
-  };
-}
+export const resetDetail = () => ({ type: RESET_DETAIL });
 
-export function postVideogame(payload) {
-  return async function (dispatch) {
-    const videogameCreate = await axios.post(`/videogames`, payload);
-    // console.log(videogameCreate);
-    return videogameCreate;
-  };
-}
+export const goBackHome = () => ({ type: GO_BACK_HOME });
 
-export function getPlatforms() {
-  return async function (dispatch) {
-    let platforms = await axios(`/platforms`);
+export const resetVideogames = () => ({ type: RESET_VIDEOGAMES });
 
-    return dispatch({
-      type: GET_PLATFORMS,
-      payload: platforms.data,
-    });
-  };
-}
+export const fromByNameToAllVideogames = () => ({
+  type: FROM_BY_NAME_TO_ALLVIDEOGAMES,
+});
 
-export function goBackHome() {
-  return {
-    type: GO_BACK_HOME,
-  };
-}
-
-export function resetVideogames() {
-  return {
-    type: RESET_VIDEOGAMES,
-  };
-}
-
-export function fromByNameToAllVideogames() {
-  return {
-    type: FROM_BY_NAME_TO_ALLVIDEOGAMES,
-  };
-}
-
-export function memoryCurrentPage(page) {
-  console.log(page);
-  return {
-    type: MEMORY_CURRENT_PAGE,
-    payload: page,
-  };
-}
+export const memoryCurrentPage = (page) => ({
+  type: MEMORY_CURRENT_PAGE,
+  payload: page,
+});
