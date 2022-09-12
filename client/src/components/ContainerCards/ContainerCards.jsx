114cef8a32e8ad782a486af6599aa63f850c7d2c
William Garcia <william_garcia_m@outlook.com>
2022-09-11 23:16:13 -0500

    add-and-remove-return-in-ContainerCards_component

--- client/src/components/ContainerCards/ContainerCards.jsx
+++ client/src/components/ContainerCards/ContainerCards.jsx
@@ -3,7 +3,7 @@ import { Card } from "../Card/Card";
 import style from "./ContainerCards.module.css";
 import { Link } from "react-router-dom";
 import { fromByNameToAllVideogames } from "../../actions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function ContainerCards({ currentVideogames, allVideogames }) {
   const dispatch = useDispatch();
@@ -12,9 +12,13 @@ export default function ContainerCards({ currentVideogames, allVideogames }) {
     dispatch(fromByNameToAllVideogames());
   };
 
+  const videogamesByName = useSelector((state) => state.videogamesByName);
+
   return (
     <div className={style.containerVideogamesByName}>
-      {allVideogames.length <= 15 && allVideogames.length >= 1 ? (
+      {allVideogames.length <= 15 &&
+      allVideogames.length >= 1 &&
+      videogamesByName.length ? (
         <button
           type="button"
           className={style.returnAllVideogames}
